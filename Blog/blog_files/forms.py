fe54463e56b4c663cd0bcbc2324500d7daa68eac
Ally <allylyes@gmail.com>
2021-03-03 20:16:27 +0000

    Changed snippit functionality so now it is something you add yourself in when creating a post... seperate from the main body of text - this will stop HTML and images from appearing and messing with the index view.

--- Blog/blog_files/forms.py
+++ Blog/blog_files/forms.py
@@ -10,7 +10,7 @@ for category in category_choices:
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'category', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippit')
 
         widgets = {
 
@@ -19,19 +19,21 @@ class PostForm(forms.ModelForm):
             'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(choices = category_choices, attrs={'class': 'form-control capitalise'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippit': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Snippit for displaying on the index page..'}),
 
         }
 
 class UpdateForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'category', 'body')
+        fields = ('title', 'title_tag', 'category', 'body', 'snippit')
 
         widgets = {
 
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title for webpage tab'}),
-            'category': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices = category_choices, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippit': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Snippit for displaying on the index page..'}),
 
         }
--- Blog/blog_files/models.py
+++ Blog/blog_files/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -19,9 +20,11 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    #body = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     category = models.CharField(max_length=255, default='coding')
+    snippit = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
