3a69f4ab2d1d747c3800293dccfc22991ed731b3
Ally <allylyes@gmail.com>
2021-03-03 15:40:52 +0000

    Added new app to host information about members of the site, ie. admin, staff, etc. Added a login function.

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_files.urls')),
+    path('staff/', include('django.contrib.auth.urls')),
+    path('', include('staff.urls')),
 ]
