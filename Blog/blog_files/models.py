2bbbf702bc4760a41dbf6ad0ba4d741917ea8570
Ally <allylyes@gmail.com>
2021-03-03 13:40:56 +0000

    Updated the form for posting a new blog on the site to make it look better.

--- Blog/blog_files/models.py
+++ Blog/blog_files/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    title_tag = models.CharField(max_length=255, default="Title for webpage tab")
+    title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
--- Blog/blog_files/views.py
+++ Blog/blog_files/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 class HomeView(ListView):
     model = Post
@@ -12,7 +13,5 @@ class PostDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
-    #Use below code to select fields instead of using all
-    #fields = ('title', 'body')
