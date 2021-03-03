2483b569b29e3cc0913909b47a4bd4af739fd6af
Ally <allylyes@gmail.com>
2021-03-03 16:35:59 +0000

    Added categories and after noticing they appear as completely lowercase on the front end, I added some CSS to change this.

--- Blog/blog_files/forms.py
+++ Blog/blog_files/forms.py
@@ -1,16 +1,19 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+category_choices = Category.objects.all().values_list('name','name')
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
 
         widgets = {
 
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title for webpage tab'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices = category_choices, attrs={'class': 'form-control capitalise'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
 
         }
@@ -18,12 +21,13 @@ class PostForm(forms.ModelForm):
 class UpdateForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'body')
+        fields = ('title', 'title_tag', 'category', 'body')
 
         widgets = {
 
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title for webpage tab'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
 
         }
--- Blog/blog_files/models.py
+++ Blog/blog_files/models.py
@@ -3,12 +3,25 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        #Uncomment below to return the post to the newly created post's page
+        #return reverse('post-details', args=(str(self.id)))
+        return reverse('home')
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=255, default='coding')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
