6be1e83267505829abdcf609d158945aaa60fac4
Ally <allylyes@gmail.com>
2021-03-03 14:24:25 +0000

    Added pagination to the index page where you view the blogs.

--- Blog/blog_files/views.py
+++ Blog/blog_files/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 from .forms import PostForm, UpdateForm
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 class HomeView(ListView):
     model = Post
+    paginate_by = 5
     template_name = 'index.html'
 
 class PostDetailView(DetailView):
